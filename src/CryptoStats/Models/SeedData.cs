7f5ba2af9cf35ddff11440ee3cdfacc33eea2454
Alexandre Costa <alex.callum.c@gmail.com>
2018-05-16 00:11:11 +0100

    feat: start implementing GDAX client on seedData

--- src/CryptoStats/Models/SeedData.cs
+++ src/CryptoStats/Models/SeedData.cs
@@ -2,11 +2,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Linq;
+using CryptoStats.GDAX;
 
 namespace CryptoStats.Models
 {
     public static class SeedData
     {
+        private GDAXClient client = new GDAXClient();
         public static void Initialize(IServiceProvider serviceProvider)
         {
             using(var context = new CryptoContext(serviceProvider.GetRequiredService<DbContextOptions<CryptoContext>>()))
