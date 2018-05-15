94eedbc1bc20f1fc402e6763986f1680e796d481
Alexandre Costa <alex.callum.c@gmail.com>
2018-05-15 13:44:46 +0100

    feat: do highestlatest and avgDiff controllers

--- src/CryptoStats/Controllers/StatsController.cs
+++ src/CryptoStats/Controllers/StatsController.cs
@@ -44,7 +44,7 @@ namespace CryptoStats.Controllers
 
         //Action to get all the stats with a specific start date
         [HttpGet]
-        public ActionResult<List<Stat>> GetAll([FromBody] DateTime startDate)
+        public ActionResult<List<Stat>> GetByStartDate([FromBody] DateTime startDate)
         {
             var items = _context.Stats.Where(s => s.startDate == startDate).ToList();
             if(items == null)
@@ -64,6 +64,104 @@ namespace CryptoStats.Controllers
                 return NotFound();
             }
             return items;
+        }
+
+        [HttpGet]
+        public ActionResult<Stat> GetByDates([FromBody] DateTime startDate, [FromBody] DateTime endDate)
+        {
+            var item = _context.Stats.Where(s => s.startDate == startDate && s.endDate == endDate).First();
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        //Find HighestLatestDiff by using a different Get URL
+        //api/stats/highestlatest/id or dates as parameters
+        [HttpGet("/highestlatest/{id}", Name="GetHighestLatestID")]
+        public ActionResult<double> GetHighestLatestByID(int id)
+        {
+            var item = _context.Stats.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item.HighestLatestDiff;
+        }
+
+        [HttpGet("/highestlatest")]
+        public ActionResult<double> GetHighestLatestByEndDate([FromBody] DateTime endDate)
+        {
+            var item = _context.Stats.Where(s => s.endDate == endDate).Last();
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item.HighestLatestDiff;
+        }
+
+        [HttpGet("/highestlatest")]
+        public ActionResult<double> GetHighestLatestByDates([FromBody] DateTime startDate, [FromBody] DateTime endDate)
+        {
+            var item = _context.Stats.Where(s => s.startDate == startDate && s.endDate == endDate).First();
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item.HighestLatestDiff;
+        }
+
+        //Get avgDiff values from stats using different GET URL
+        //Example api/stats/avgdiff/{id} or dates
+
+        [HttpGet("/avgdiff/{id}", Name="GetAvgDiffByID")]
+        public ActionResult<double> GetAvgDiffByID(int id)
+        {
+            var item = _context.Stats.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item.avgDiff;
+        }
+
+        //To get AvgDiff from startDate, all the avgDiff from the
+        //start date onwards are put on a list and the overall avg is
+        //calculated
+        [HttpGet("/avgdiff")]
+        public ActionResult<double> GetAvgDiffByStartDate([FromBody] DateTime startDate)
+        {
+            var items = _context.Stats.Where(s => s.startDate == startDate).ToList();
+            if(items == null)
+            {
+                return NotFound();
+            }
+            double avgs = 0.00;
+            foreach(Stat s in items)
+            {
+                avgs += s.avgDiff;
+            }
+            double avgDiff = avgs / items.Count();
+            return avgDiff;
+        }
+
+        //To get the avgDiff from endDate, the same procedure as for from startDate is applied: avg of avgs
+        [HttpGet("/avgdiff")]
+        public ActionResult<double> GetAvgDiffByEndDate([FromBody] DateTime endDate)
+        {
+            var items = _context.Stats.Where(s => s.endDate == endDate).ToList();
+            if(items == null)
+            {
+                return NotFound();
+            }
+            double avgs = 0.00;
+            foreach(Stat s in items)
+            {
+                avgs += s.avgDiff;
+            }
+            double avgDiff = avgs / items.Count();
+            return avgDiff;
         }
     }
 }
