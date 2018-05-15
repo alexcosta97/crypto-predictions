8950a4082d9e1f0f0200a07e52dbd117c28c740f
Alexandre Costa <alex.callum.c@gmail.com>
2018-05-15 14:08:10 +0100

    feat: finish avgGrowth and start avgDecline controllers

--- src/CryptoStats/Controllers/StatsController.cs
+++ src/CryptoStats/Controllers/StatsController.cs
@@ -163,5 +163,89 @@ namespace CryptoStats.Controllers
             double avgDiff = avgs / items.Count();
             return avgDiff;
         }
+
+        //The same procedure is applied for an id search is applied when looking for the avgDiff with a start and end dates
+        [HttpGet("/avgdiff")]
+        public ActionResult<double> GetAvgDiffByDates([FromBody] DateTime startDate, [FromBody] DateTime endDate)
+        {
+            var item = _context.Stats.Where(s => s.startDate == startDate && s.endDate == endDate).First();
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item.avgDiff;
+        }
+
+        //Get avgGrowth values from stats using different GET URL
+        //Example api/stats/avggrowth/{id} or dates as parameters
+        [HttpGet("/avggrowth/{id}", Name="GetAvgGrowthByID")]
+        public ActionResult<long> GetAvgGrowthByID(int id)
+        {
+            var item = _context.Stats.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item.avgGrowthTime;
+        }
+
+        //To get the avgGrowthTime for a start or end date, the same procedure as for the avgDiff is applied, meaning doing an avg of avgs
+        [HttpGet("/avggrowth")]
+        public ActionResult<long> GetAvgGrowthByStartDate([FromBody] DateTime startDate)
+        {
+            var items = _context.Stats.Where(s => s.startDate == startDate).ToList();
+            if(items == null)
+            {
+                return NotFound();
+            }
+            long times = 0;
+            foreach(Stat s in items)
+            {
+                times += s.avgGrowthTime;
+            }
+            return times / items.Count();
+        }
+
+        [HttpGet("/avggrowth")]
+        public ActionResult<long> GetAvgGrowthByEndDate([FromBody] DateTime endDate)
+        {
+            var items = _context.Stats.Where(s => s.endDate == endDate).ToList();
+            if(items == null)
+            {
+                return NotFound();
+            }
+            long times = 0;
+            foreach(Stat s in items)
+            {
+                times += s.avgGrowthTime;
+            }
+            return times / items.Count();
+        }
+
+        [HttpGet("/avggrowth")]
+        public ActionResult<long> GetAvgGrowthByDates([FromBody] DateTime startDate, [FromBody] DateTime endDate)
+        {
+            var item = _context.Stats.Where(s => s.startDate == startDate && s.endDate == endDate).First();
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item.avgGrowthTime;
+        }
+
+        //To get the avgDecline values, same procedures apply as for avgGrowth
+        //URLs are built the same way as for avgDecline, but with avgdecline instead of avggrowth
+        [HttpGet("/avgdecline/{id}", Name="GetAvgDeclineByID")]
+        public ActionResult<long> GetAvgDeclineByID(int id)
+        {
+            var item = _context.Stats.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item.avgDeclineTime;
+        }
+        
+        [HttpGet("/avgdecline")]
     }
 }
