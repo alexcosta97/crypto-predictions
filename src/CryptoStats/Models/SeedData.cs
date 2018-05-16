8a12f764ebeabb9e7ce711e5a1849fd9f43c9f51
Alexandre Costa <alex.callum.c@gmail.com>
2018-05-16 14:36:12 +0100

    feat: implement independent seedStat and send context to seedData class

--- src/CryptoStats/Models/SeedData.cs
+++ src/CryptoStats/Models/SeedData.cs
@@ -37,201 +37,16 @@ namespace CryptoStats.Models
                 {
                     foreach(Exchange e in context.Exchanges)
                     {
-                        foreach(Product p in client.ProductsService.GetAllProductsAsync().Result)
-                        {
-                            if(p.Id.ToString().Equals(e.Name))
-                            {
-                                DateTime startDate = DateTime.Now.AddDays(-7);
-                                DateTime endDate = DateTime.Now;
-                                for(int i = 0; i < 7; i++)
-                                {
-                                    var candles = client.ProductsService.GetHistoricRatesAsync(p.Id, startDate, endDate, CandleGranularity.Minutes1).Result;
-                                    Stat adder = new Stat(){
-                                        startDate = candles.First().Time,
-                                        endDate = candles.Last().Time  
-                                    };
-
-                                    //calculate highestLatestDiff
-                                    decimal high = 0.00M;
-                                    foreach(Candle c in candles)
-                                    {
-                                        if(c.High > high)
-                                        {
-                                            high = (decimal)c.High;
-                                        }
-                                    }
-                                    adder.HighestLatestDiff = (decimal)candles.Last().Close - high;
-
-                                    //calculate avgDiff
-                                    //Use difference between open and close of candles to calculate the average diff
-                                    decimal candlesDiff = 0.00M;
-                                    foreach(Candle c in  candles)
-                                    {
-                                        candlesDiff += (decimal)(c.Close - c.Open);
-                                    }
-                                    adder.avgDiff = candlesDiff / candles.Count();
-                                    
-                                    //calculate average growth and decline times
-                                    //store candles in a temporary list to retrieve their times when not in growth/decline anymore
-                                    //growth and decline times are stored in a list of timespans
-                                    //average is calculated afterwards
-                                    List<TimeSpan> growthTimes = new List<TimeSpan>();
-                                    List<TimeSpan> declineTimes = new List<TimeSpan>();
-                                    bool inGrowth = false;
-                                    List<Candle> growthCandles = new List<Candle>();
-
-                                    foreach(Candle c in candles)
-                                    {
-                                        if(c.Close > c.Open && inGrowth == false)
-                                        {
-                                            inGrowth = true;
-                                            declineTimes.Add(growthCandles.Last().Time.Subtract(growthCandles.First().Time));
-                                            growthCandles.Clear();
-                                            growthCandles.Add(c);
-                                        }
-                                        else if(c.Close > c.Open && inGrowth == true)
-                                        {
-                                            growthCandles.Add(c);
-                                        }
-                                        else if(c.Close < c.Open && inGrowth == true)
-                                        {
-                                            inGrowth = false;
-                                            growthTimes.Add(growthCandles.Last().Time.Subtract(growthCandles.First().Time));
-                                            growthCandles.Clear();
-                                            growthCandles.Add(c);
-                                        }
-                                        else if(c.Close < c.Open && inGrowth == false)
-                                        {
-                                            growthCandles.Add(c);
-                                        }
-                                    }
-                                    if(inGrowth)
-                                    {
-                                        growthTimes.Add(growthCandles.Last().Time.Subtract(growthCandles.First().Time));
-                                    }
-                                    else
-                                    {
-                                        declineTimes.Add(growthCandles.Last().Time.Subtract(growthCandles.First().Time));
-                                    }
-
-                                    double totalTimes = 0;
-                                    foreach(TimeSpan t in growthTimes)
-                                    {
-                                        totalTimes += t.TotalMilliseconds;
-                                    }
-                                    adder.avgGrowthTime = TimeSpan.FromMilliseconds(totalTimes/ growthTimes.Count());
-                                    
-                                    totalTimes = 0;
-                                    foreach(TimeSpan t in declineTimes)
-                                    {
-                                        totalTimes += t.TotalMilliseconds;
-                                    }
-                                    adder.avgDeclineTime = TimeSpan.FromMilliseconds(totalTimes / declineTimes.Count());
-                                    context.Stats.Add(adder);
-                                    context.SaveChangesAsync();
-                                    startDate.AddDays(1);
-                                }
-
-                                startDate.AddDays(-7);
-                                for(int i = 0; i < 7; i++)
-                                {
-                                    var candles = client.ProductsService.GetHistoricRatesAsync(p.Id, startDate, endDate, CandleGranularity.Minutes1).Result;
-                                    Stat adder = new Stat(){
-                                        startDate = candles.First().Time,
-                                        endDate = candles.Last().Time  
-                                    };
-
-                                    //calculate highestLatestDiff
-                                    decimal high = 0.00M;
-                                    foreach(Candle c in candles)
-                                    {
-                                        if(c.High > high)
-                                        {
-                                            high = (decimal)c.High;
-                                        }
-                                    }
-                                    adder.HighestLatestDiff = (decimal)candles.Last().Close - high;
-
-                                    //calculate avgDiff
-                                    //Use difference between open and close of candles to calculate the average diff
-                                    decimal candlesDiff = 0.00M;
-                                    foreach(Candle c in  candles)
-                                    {
-                                        candlesDiff += (decimal)(c.Close - c.Open);
-                                    }
-                                    adder.avgDiff = candlesDiff / candles.Count();
-                                    
-                                    //calculate average growth and decline times
-                                    //store candles in a temporary list to retrieve their times when not in growth/decline anymore
-                                    //growth and decline times are stored in a list of timespans
-                                    //average is calculated afterwards
-                                    List<TimeSpan> growthTimes = new List<TimeSpan>();
-                                    List<TimeSpan> declineTimes = new List<TimeSpan>();
-                                    bool inGrowth = false;
-                                    List<Candle> growthCandles = new List<Candle>();
-
-                                    foreach(Candle c in candles)
-                                    {
-                                        if(c.Close > c.Open && inGrowth == false)
-                                        {
-                                            inGrowth = true;
-                                            declineTimes.Add(growthCandles.Last().Time.Subtract(growthCandles.First().Time));
-                                            growthCandles.Clear();
-                                            growthCandles.Add(c);
-                                        }
-                                        else if(c.Close > c.Open && inGrowth == true)
-                                        {
-                                            growthCandles.Add(c);
-                                        }
-                                        else if(c.Close < c.Open && inGrowth == true)
-                                        {
-                                            inGrowth = false;
-                                            growthTimes.Add(growthCandles.Last().Time.Subtract(growthCandles.First().Time));
-                                            growthCandles.Clear();
-                                            growthCandles.Add(c);
-                                        }
-                                        else if(c.Close < c.Open && inGrowth == false)
-                                        {
-                                            growthCandles.Add(c);
-                                        }
-                                    }
-                                    if(inGrowth)
-                                    {
-                                        growthTimes.Add(growthCandles.Last().Time.Subtract(growthCandles.First().Time));
-                                    }
-                                    else
-                                    {
-                                        declineTimes.Add(growthCandles.Last().Time.Subtract(growthCandles.First().Time));
-                                    }
-
-                                    double totalTimes = 0;
-                                    foreach(TimeSpan t in growthTimes)
-                                    {
-                                        totalTimes += t.TotalMilliseconds;
-                                    }
-                                    adder.avgGrowthTime = TimeSpan.FromMilliseconds(totalTimes/ growthTimes.Count());
-                                    
-                                    totalTimes = 0;
-                                    foreach(TimeSpan t in declineTimes)
-                                    {
-                                        totalTimes += t.TotalMilliseconds;
-                                    }
-                                    adder.avgDeclineTime = TimeSpan.FromMilliseconds(totalTimes / declineTimes.Count());
-                                    context.Stats.Add(adder);
-                                    context.SaveChangesAsync();
-                                    endDate.AddDays(-1);
-                                }
-                            }
-                        }
+                        seedStat(e.ExchangeId, DateTime.Now.AddDays(-7), DateTime.Now, context);
                     }
                     
                 }
             }
         }
 
-        public static void seedStat(int exchangeID, DateTime startDate, DateTime endDate)
+        public static void seedStat(int exchangeID, DateTime startDate, DateTime endDate, CryptoContext context)
         {
-            using(var context = new CryptoContext(serviceProvider.GetRequiredService<DbContextOptions<CryptoContext>>()))
+            using(context)
             {
                 foreach(Product p in client.ProductsService.GetAllProductsAsync().Result)
                 {
@@ -311,14 +126,14 @@ namespace CryptoStats.Models
                                     }
 
                                     double totalTimes = 0;
-                                    foreach(TimeSpan t in growthTimes)
+                                    foreach(TimeSpan time in growthTimes)
                                     {
                                         totalTimes += t.TotalMilliseconds;
                                     }
                                     adder.avgGrowthTime = TimeSpan.FromMilliseconds(totalTimes/ growthTimes.Count());
                                     
                                     totalTimes = 0;
-                                    foreach(TimeSpan t in declineTimes)
+                                    foreach(TimeSpan time in declineTimes)
                                     {
                                         totalTimes += t.TotalMilliseconds;
                                     }
@@ -401,14 +216,14 @@ namespace CryptoStats.Models
                                     }
 
                                     double totalTimes = 0;
-                                    foreach(TimeSpan t in growthTimes)
+                                    foreach(TimeSpan time in growthTimes)
                                     {
                                         totalTimes += t.TotalMilliseconds;
                                     }
                                     adder.avgGrowthTime = TimeSpan.FromMilliseconds(totalTimes/ growthTimes.Count());
                                     
                                     totalTimes = 0;
-                                    foreach(TimeSpan t in declineTimes)
+                                    foreach(TimeSpan time in declineTimes)
                                     {
                                         totalTimes += t.TotalMilliseconds;
                                     }
--- src/CryptoStats/Program.cs
+++ src/CryptoStats/Program.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using CryptoStats.Models;
 
 namespace CryptoStats
 {
@@ -14,11 +16,29 @@ namespace CryptoStats
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .Build();
     }
 }
